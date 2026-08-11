b9ade9593804f87bc38485a5481da754ce775dd1
manimalladi <manimalladi05@gmail.com>
2026-08-11 11:38:08 +0530

    meta title ,secription add

--- navbar.php
+++ navbar.php
@@ -123,7 +123,18 @@
     </script>
 
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-L7L9DE4T77"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-L7L9DE4T77');
+    </script>
 
 
 </head>
